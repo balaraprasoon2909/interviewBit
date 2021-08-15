160632f2d674e672a2be5c1e9edb416028064ac3
Prasoon Balara <69413205+balaraprasoon2909@users.noreply.github.com>
2021-08-15 17:56:57 +0530

    Update 3Sum.java

--- 3Sum.java
+++ 3Sum.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int threeSumClosest(int[] nums, int sum) {
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[nums.length-1];
+        for(int i=0; i<nums.length-2; i++){
+            int j = i+1;
+            int k = nums.length-1;
+            while(j < k){
+                int temp = nums[i] + nums[j] + nums[k];
+                if(temp == sum) return temp;
+                if(temp > sum) k--;
+                if(temp < sum) j++;
+                if(Math.abs(temp-sum) < Math.abs(result-sum)) result = temp;
+            }
+        }
+        return result;
+    }
+}
